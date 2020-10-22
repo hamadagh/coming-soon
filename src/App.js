3f793330cf7dea566bea2c333987f0de2bceee25
hamada <hamadagh777@gmail.com>
2020-10-23 00:55:04 +0200

    adding image and icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import img from "./assets/hand.png";
+import facebook from "./assets/facebook.png";
+import twitter from "./assets/twitter.png";
+import telegram from "./assets/telegram.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="left__side"></div>
+      <div className="left__side">
+        <img src={img} alt="hand" className="hand__image" />
+      </div>
       <div className="right__side">
         <div>
           <span className="first__header">10% Discount</span>
@@ -20,7 +26,12 @@ function App() {
             <button>Notify me</button>
           </div>
           <div className="icons__wrapper">
-            <p>Get in touch</p>
+            <p className="icons__header">Get in touch</p>
+            <div className="icons">
+              <img src={facebook} alt="facebook" width="25" height="25" />
+              <img src={twitter} alt="twitter" width="25" height="25" />
+              <img src={telegram} alt="telegram" width="25" height="25" />
+            </div>
           </div>
         </div>
       </div>
